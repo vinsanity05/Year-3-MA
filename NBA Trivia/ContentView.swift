335dcd45f1f7efb144d3332d0d088d31a64ad5a6
Vince Verdadero <chroviver3@gmail.com>
2022-03-17 21:01:33 +0000

    Initial Commit

---
+++ NBA Trivia/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NBA Trivia
+//
+//  Created by Vince Verdadero on 17/03/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NBA Trivia/NBA_TriviaApp.swift
@@ -0,0 +1,17 @@
+//
+//  NBA_TriviaApp.swift
+//  NBA Trivia
+//
+//  Created by Vince Verdadero on 17/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct NBA_TriviaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
